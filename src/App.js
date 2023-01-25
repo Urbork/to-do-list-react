1641bc7d31841ef2bc4fcea3d89300efc03ac47d
Jonasz Ja <jajonaszjj@gmail.com>
2023-01-25 12:34:34 +0100

    if changed to ternary

--- src/App.js
+++ src/App.js
@@ -23,13 +23,9 @@ function App() {
 
   const toggleTaskDone = (id) => {
     setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
-
-        return task;
-      })
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
     );
   };
 
