c001a87be9a0c76d1fc5d5473eb8b78def3247d4
Jonasz Jachym <jajonaszjj@gmail.com>
2023-09-29 09:16:12 +0200

    application split into features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Form from "./Form";
+import List from "./List";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+      <Section
+        title="Lista zadań"
+        body={
+          <List
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
