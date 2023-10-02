a4a0d7f5ce86c91aadb390bcbd3e55e411eee03d
Jonasz Jachym <jajonaszjj@gmail.com>
2023-10-02 14:05:52 +0200

    taskSlice added

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: {
+    tasks: [
+      {
+        id: 1,
+        content: "test",
+        done: true,
+      },
+    ],
+    hideDone: false,
+  },
+  reducers: {
+    addTask: ({ tasks }, { payload }) => {
+      tasks.push(payload);
+    },
+    toggleHideDone: (state) => {
+      state.hideDone = !state.hideDone;
+    },
+  },
+});
+
+export const { addTask, toggleHideDone } = tasksSlice.actions;
+export const selectTasks = (state) => state.tasks;
+export default tasksSlice.reducer;
+
+console.log(
+  addTask({
+    content: "Nauczyć się Reduxa i Reacta",
+    done: false,
+    id: 999,
+  })
+);
+
+console.log(
+  tasksSlice.reducer(
+    { tasks: [] },
+    addTask({
+      content: "Test123",
+      done: true,
+    })
+  )
+);
