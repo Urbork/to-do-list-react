0b40b96b4a89964b3f39e0d97530c63b5c532864
Jonasz Jachym <jajonaszjj@gmail.com>
2023-12-16 08:14:51 +0100

    taskSlice - removeTask added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -14,7 +14,7 @@ const tasksSlice = createSlice({
   },
   reducers: {
     addTask: ({ tasks }, { payload }) => {
-      tasks.push(payload);
+      tasks.unshift(payload);
     },
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
@@ -27,10 +27,16 @@ const tasksSlice = createSlice({
       state.tasks[index].done = !state.tasks[index].done;
       // console.log(action.payload);
     },
+    removeTask: (state, action) => {
+      const index = state.tasks.findIndex((task) => task.id === action.payload);
+      state.tasks.splice(index, 1);
+      // console.log(action.payload);
+    },
   },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask } =
+  tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
 
