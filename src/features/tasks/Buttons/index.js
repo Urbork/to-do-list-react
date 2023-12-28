bc7d451222165fea36bf58bc3bdef3b8a44a5387
Jonasz Jachym <jajonaszjj@gmail.com>
2023-12-28 18:12:37 +0100

    Buttons updated

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,16 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
 import { Wrapper, Button } from "./styled.js";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice.js";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => {
+const Buttons = () => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+
   if (tasks.length === 0) {
     return null;
   }
 
   return (
     <Wrapper>
-      <Button onClick={toggleHideDone}>
+      <Button onClick={() => dispatch(toggleHideDone())}>
         {hideDone ? "Pokaż" : "Ukryj"} ukończone
       </Button>
-      <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}>
+      {/* <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}> */}
+      <Button
+        onClick={() => dispatch(setAllDone())}
+        disabled={tasks.every(({ done }) => done)}
+      >
         Ukończ wszystkie
       </Button>
     </Wrapper>
