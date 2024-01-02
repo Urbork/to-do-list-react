35fb1e12c9a01b4c671806c9733ea4a75605eb12
Jonasz Jachym <jajonaszjj@gmail.com>
2024-01-02 17:08:35 +0100

    comments removed

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,60 +1,19 @@
-// import { useSelector } from "react-redux";
-// import { useState } from "react";
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
-// import { useTasks } from "../../useTasks";
-// import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-  // const [hideDone, setHideDone] = useState(false);
-
-  // const toggleHideDone = () => {
-  //   setHideDone((hideDone) => !hideDone);
-  // };
-
-  // const { tasks } = useSelector(selectTasks);
-
-  // const {
-  //   tasks,
-  //   removeTask,
-  //   toggleTaskDone,
-  //   setAllDone,
-  //   addNewTask,
-  // } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={
-          <Form
-          // addNewTask={addNewTask}
-          />
-        }
-      />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section
         title="Lista zadań"
-        body={
-          <List
-          // tasks={tasks}
-          // hideDone={hideDone}
-          // removeTask={removeTask}
-          // toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-          // tasks={tasks}
-          // hideDone={hideDone}
-          // toggleHideDone={toggleHideDone}
-          // setAllDone={setAllDone}
-          />
-        }
+        body={<List />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
