609106e685d519494db263aee0509e15f82dd67b
Jonasz Jachym <jajonaszjj@gmail.com>
2024-01-04 14:29:45 +0100

    Form updated

--- src/features/tasks/Form/index.js
+++ src/features/tasks/Form/index.js
@@ -1,18 +1,32 @@
 import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 import { MainForm, FormInput, FormButton } from "./styled";
+import { addTask } from "../tasksSlice";
 
-const Form = ({ addNewTask }) => {
+const Form = () => {
   const [newTaskContent, setNewTaskContent] = useState("");
   const focusRef = useRef(null);
 
+  const dispatch = useDispatch();
+
   const onFormSubmit = (event) => {
     event.preventDefault();
+    const trimmedNewTaskContent = newTaskContent.trim();
+    const newTaskId = nanoid();
 
-    if (newTaskContent.trim() === "") {
+    if (trimmedNewTaskContent === "") {
       return;
     }
 
-    addNewTask(newTaskContent.trim());
+    dispatch(
+      addTask({
+        content: trimmedNewTaskContent,
+        done: false,
+        id: newTaskId,
+      })
+    );
+
     setNewTaskContent("");
     focusRef.current.focus();
   };
